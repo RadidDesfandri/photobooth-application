0f5ed30aa3ff4229a6a2765a5814b6a18711e3a3
RadidDesfandri <radietdesfandri@gmail.com>
2025-12-17 13:49:06 +0700

    Setup lint-staged

---
+++ src/renderer/src/layouts/main-layout.tsx
@@ -0,0 +1,7 @@
+import { JSX } from 'react'
+
+function MainLayout(): JSX.Element {
+  return <div>MainLayout</div>
+}
+
+export default MainLayout
--- src/renderer/src/layouts/navigation.tsx
+++ src/renderer/src/layouts/navigation.tsx
@@ -1,15 +1,28 @@
-import { Outlet, Link } from 'react-router-dom'
+import { JSX } from 'react'
+import { Outlet, Link, useLocation } from 'react-router-dom'
 
-const Navigation = () => {
+const mappingToTitle = {
+  '': 'Home',
+  'live-capture': 'Live Capture'
+}
+
+const Navigation = (): JSX.Element => {
+  const location = useLocation()
+
   return (
     <div>
-      <nav className="flex items-center gap-4 justify-center p-4 bg-gray-100 mb-3">
-        <Link to="/">Home</Link> | <Link to="/live-capture">Capture</Link> |{' '}
-        <Link to="/settings">Settings</Link>
+      <nav className="flex items-center justify-between px-8 p-4 mb-3 bg-neutral-900 text-white shadow-md">
+        <h1 className="font-medium text-3xl">
+          {mappingToTitle[location.pathname.split('/')[1]] || 'Unknown Page'}
+        </h1>
+
+        <div className="space-x-4">
+          <Link to="/">Home</Link>
+          <Link to="/live-capture">Live Capture</Link>
+        </div>
       </nav>
 
       <main>
-        {/* Outlet adalah tempat halaman-halaman (children) dirender */}
         <Outlet />
       </main>
     </div>
